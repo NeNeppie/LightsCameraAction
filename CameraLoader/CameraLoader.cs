93670d44633ca6b8b8afc87796bd597640e29d43
NeNeppie <108415463+NeNeppie@users.noreply.github.com>
2023-02-01 00:36:28 +0200

    Create template

---
+++ CameraLoader/CameraLoader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dalamud.Plugin;
+
+public class CameraLoader : IDalamudPlugin
+{
+    public string Name => "CameraLoader?";
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
